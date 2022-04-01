47b29cc7c6a4fe721180030d20cfded3c6d3fd62
Janani harshu <janjanani13@gmail.com>
2022-04-01 21:15:31 +0530

    Add files via upload

---
+++ 2 Single Vs Multithread Visualisation/AshycMultiThreadingProject/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace AshycMultiThreadingProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSingleTadhr = new System.Windows.Forms.Button();
+            this.panelSingleTread = new System.Windows.Forms.Panel();
+            this.panelMultithreading = new System.Windows.Forms.Panel();
+            this.buttonMultiThread = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonSingleTadhr
+            // 
+            this.buttonSingleTadhr.Location = new System.Drawing.Point(42, 10);
+            this.buttonSingleTadhr.Name = "buttonSingleTadhr";
+            this.buttonSingleTadhr.Size = new System.Drawing.Size(200, 20);
+            this.buttonSingleTadhr.TabIndex = 1;
+            this.buttonSingleTadhr.Text = "Run With Single Thread";
+            this.buttonSingleTadhr.UseVisualStyleBackColor = true;
+            this.buttonSingleTadhr.Click += new System.EventHandler(this.buttonSingleThread_Click);
+            // 
+            // panelSingleTread
+            // 
+            this.panelSingleTread.BackColor = System.Drawing.Color.Black;
+            this.panelSingleTread.Location = new System.Drawing.Point(12, 36);
+            this.panelSingleTread.Name = "panelSingleTread";
+            this.panelSingleTread.Size = new System.Drawing.Size(250, 250);
+            this.panelSingleTread.TabIndex = 15;
+            // 
+            // panelMultithreading
+            // 
+            this.panelMultithreading.BackColor = System.Drawing.Color.Black;
+            this.panelMultithreading.Location = new System.Drawing.Point(268, 36);
+            this.panelMultithreading.Name = "panelMultithreading";
+            this.panelMultithreading.Size = new System.Drawing.Size(250, 250);
+            this.panelMultithreading.TabIndex = 16;
+            // 
+            // buttonMultiThread
+            // 
+            this.buttonMultiThread.Location = new System.Drawing.Point(289, 10);
+            this.buttonMultiThread.Name = "buttonMultiThread";
+            this.buttonMultiThread.Size = new System.Drawing.Size(200, 20);
+            this.buttonMultiThread.TabIndex = 17;
+            this.buttonMultiThread.Text = "Run With Mulit-Thread";
+            this.buttonMultiThread.UseVisualStyleBackColor = true;
+            this.buttonMultiThread.Click += new System.EventHandler(this.buttonMultiThread_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 19;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 289);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 31);
+            this.panel1.TabIndex = 20;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Silver;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(267, 289);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(250, 31);
+            this.panel2.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 327);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonMultiThread);
+            this.Controls.Add(this.panelMultithreading);
+            this.Controls.Add(this.panelSingleTread);
+            this.Controls.Add(this.buttonSingleTadhr);
+            this.Name = "Form1";
+            this.Text = "Testing Environement";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSingleTadhr;
+        private System.Windows.Forms.Panel panelSingleTread;
+        private System.Windows.Forms.Panel panelMultithreading;
+        private System.Windows.Forms.Button buttonMultiThread;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ 2 Single Vs Multithread Visualisation/AshycMultiThreadingProject/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AshycMultiThreadingProject
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch watchSingle = new Stopwatch();
+        Stopwatch watchMulti = new Stopwatch();
+        CancellationTokenSource ctsSingle = new CancellationTokenSource();// act like an event to notice the cancelation
+        CancellationTokenSource ctsMulti = new CancellationTokenSource();// act like an event to notice the cancelation
+        Task singleTask = new Task(() => { });// task use for the single task
+        List<Task> tasks = new List<Task>();// collection of task that represent the multitask we gonna do
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Reset le grid system to gray
+        /// </summary>
+        /// <param name="panel"></param>
+        public void resetGrid(Panel panel)
+        {
+            foreach (Panel p in panel.Controls)
+            {
+
+                if (p is Panel)
+                {
+
+                    p.BackColor = Color.Gray;
+
+                    if (!this.InvokeRequired)
+                    {
+                        p.Update();
+                    }
+
+                }
+
+            }
+        }
+        /// <summary>
+        /// Simulate a heavy process 
+        /// </summary>
+        /// <param name="op"></param>
+        public void prossess(object op)
+        {
+
+            Panel p = (Panel)op;
+            p.BackColor = Color.Green;
+            int a = 0 ;
+
+            for (int i = 0; i < 10000000; i++)
+            {
+                a++;
+            }
+
+            if (!this.InvokeRequired)// Enshure to not making some cross-threading error
+            {
+
+                p.Update();// update de ui
+
+            }
+        }
+        /// <summary>
+        /// Preload when the program start
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            separatePanel(panelSingleTread, 10, 10);
+            separatePanel(panelMultithreading, 10, 10);
+
+            // Separate thread that run asynchronously for making the timer update on the UI
+            ThreadPool.QueueUserWorkItem(new WaitCallback(p =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(200);
+                    label1.Invoke((Action)delegate { label1.Text = watchSingle.Elapsed.ToString(); });
+
+                }
+            }));
+            // Separate thread that run asynchronously for making the timer update on the UI
+            ThreadPool.QueueUserWorkItem(new WaitCallback(p =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(100);
+                    label2.Invoke((Action)delegate { label2.Text = watchMulti.Elapsed.ToString(); });
+
+                }
+            }));
+        }
+        /// <summary>
+        /// Generate all square in the grid
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="horSepa"></param>
+        /// <param name="vertSepa"></param>
+        public void separatePanel(Panel p, int horSepa, int vertSepa)
+        {
+            Size separationSize = new Size(p.Size.Width / horSepa, p.Size.Height / vertSepa);
+            for (int i = 0; i < horSepa; i++)
+            {
+                for (int j = 0; j < vertSepa; j++)
+                {
+                    p.Controls.Add(new Panel() { Size = new Size(separationSize.Width - 2, separationSize.Height - 2), BackColor = Color.Gray, Location = new Point((separationSize.Width * i) + 1, (separationSize.Height * j) + 1) });
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Run the single thread grid
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void buttonSingleThread_Click(object sender, EventArgs e)
+        {
+
+            switch (singleTask.Status)
+            {
+
+                case TaskStatus.Running:
+                    {
+                        ctsSingle.Cancel();
+                        await singleTask;//wait the process is done before continuing
+                        ctsSingle = new CancellationTokenSource();
+                        singleTask = new Task(() => { });
+                        buttonSingleThread_Click(sender, e);//retry the function afther cancelation
+
+                        break;
+                    }
+                case TaskStatus.Created:
+                    {
+                        watchSingle.Reset();
+                        watchSingle.Start();
+
+
+                        singleTask = new Task(() =>
+                        {
+
+
+
+                            resetGrid(panelSingleTread);
+                            foreach (Panel p in panelSingleTread.Controls)
+                            {
+                                if (ctsSingle.IsCancellationRequested)//Stop processing if canceled
+                                {
+                                    break;
+                                }
+                                if (p is Panel)
+                                {
+                                    prossess(p);
+                                }
+                            }
+
+                        }, ctsSingle.Token);
+
+                        singleTask.Start();
+
+
+                        await singleTask;//wait the process is done before continuing
+
+
+                        watchSingle.Stop();
+                        singleTask = new Task(() => { });//reset task if done
+                        break;
+                    }
+            }
+
+        }
+
+        /// <summary>
+        /// Run the multithread grid
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void buttonMultiThread_Click(object sender, EventArgs e)
+        {
+            switch (tasks.Count)
+            {
+                case 0: // 0 when task created
+                    {
+                        watchMulti.Reset();
+                        watchMulti.Start();
+                        resetGrid(panelMultithreading);
+
+                        foreach (Panel p in panelMultithreading.Controls)
+                        {
+                            if (ctsMulti.IsCancellationRequested)//Stop processing if canceled
+                            {
+                                break;
+                            }
+                            if (p is Panel)
+                            {
+                                tasks.Add(new Task(() => { prossess(p); },ctsMulti.Token)) ;
+                            }
+                        }
+                        foreach (Task t in tasks)
+                        {
+                            if (t.Status == TaskStatus.Created)
+                            {
+                                if (ctsMulti.IsCancellationRequested)
+                                {
+                                    break;
+                                }
+                                t.Start();
+                            }
+                        }
+                        try
+                        {
+                            await Task.WhenAll(tasks);//wait the process is done before continuing
+                            watchMulti.Stop();
+                            tasks = new List<Task>();
+                        }
+                        catch (OperationCanceledException)//catch exeption and let program running
+                        {
+
+                        }
+                      
+                        break;
+                    }
+                default: // when task canceled
+                    {
+                        ctsMulti.Cancel();                       
+                        await Task.WhenAll(tasks.FindAll(s => s.Status == TaskStatus.Running));//wait only the running task note : we are doing that cause a started task cant be stop
+                        tasks = new List<Task>();
+                        ctsMulti = new CancellationTokenSource();
+                        buttonMultiThread_Click(sender, e);//retry the function afther cancelation
+                        break;
+                    }
+
+            }
+
+        }
+    }
+}
